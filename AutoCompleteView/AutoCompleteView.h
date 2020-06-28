5c7cb7d15645a5c65f8ec235e13273cbef9d7fc0
Andrii Petrovskyi <a.petrovskyi@devrain.com>
2020-06-28 22:35:38 +0300

    Initial Commit

---
+++ AutoCompleteView/AutoCompleteView.h
@@ -0,0 +1,19 @@
+//
+//  AutoCompleteView.h
+//  AutoCompleteView
+//
+//  Created by Admin on 28.06.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoCompleteView.
+FOUNDATION_EXPORT double AutoCompleteViewVersionNumber;
+
+//! Project version string for AutoCompleteView.
+FOUNDATION_EXPORT const unsigned char AutoCompleteViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoCompleteView/PublicHeader.h>
+
+
